2e80f836d751b3d7cb729694fdbaec111069858e
piyushchaudhari31 <mrpiyushchaudhari2006@gmail.com>
2025-08-20 17:59:25 +0530

    delete_auth

--- Day_10/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-
-const app = require('./src/app')
-
-
-app.listen(3000,()=>{
-    console.log("Server is Running...");
-    
-})
--- Day_10/src/db/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-function connectToDb(){
-    mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("Connected to DB")).catch((error)=>console.log(error))
-}
-
-module.exports = connectToDb
